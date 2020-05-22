e508aa5e3759ef04c82344d9626d17a6b2b08e80
onyschukoleh <onyschukoleh@gmail.com>
2020-05-22 15:44:35 +0300

    Add code

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ const account = {
 };
 
 account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+console.log("#1 ", account.discount); // 0.15
 
 console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
 
 account.addOrder(5000, "order-4");
-console.log(account.balance); // 19000
+console.log("#1: ", account.balance); // 19000
 // console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
 // ===================================================
 /*module_04 Homework_04_2
@@ -34,19 +34,19 @@ console.log(account.balance); // 19000
 const inventory = {
   items: ["Knife", "Gas mask"],
   add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
+    console.log("#2: ", `Adding ${itemName} to inventory`);
 
     inventory.items.push(itemName);
   },
   remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
+    console.log("#2: ", `Removing ${itemName} from inventory`);
 
     inventory.items = inventory.items.filter((item) => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function (itemName, action) {
-  console.log(`Invoking action on ${itemName}`);
+  console.log("#2: ", `Invoking action on ${itemName}`);
   action(itemName);
 };
 
@@ -54,10 +54,10 @@ invokeInventoryAction("Medkit", inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+console.log("#2: ", inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
 invokeInventoryAction("Gas mask", inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
-console.log(inventory.items); // ['Knife', 'Medkit']
+console.log("#2: ", inventory.items); // ['Knife', 'Medkit']
